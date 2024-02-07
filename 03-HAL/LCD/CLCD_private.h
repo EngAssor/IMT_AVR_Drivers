2d34b7fc003f87ccec8a2bfa661809d7d5e9365c
Asser Tantawy <Aserfs2000@gmail.com>
2024-02-07 14:12:36 +0200

    LCD driver

---
+++ 03-HAL/LCD/CLCD_private.h
@@ -0,0 +1,19 @@
+/*
+ * LCD_private.h
+ *
+ *  Created on: Feb 6, 2024
+ *      Author: Asser Tanatwy
+ */
+
+#ifndef HAL_LCD_CLCD_PRIVATE_H_
+#define HAL_LCD_CLCD_PRIVATE_H_
+
+/****** LCD commands****/
+
+/**/
+u8 CustomeChar[]=
+		    {
+				0x00,0x00,0x00,0x00,0x07,0x04,0x1C,0x00
+			};
+
+#endif /* HAL_LCD_CLCD_PRIVATE_H_ */
